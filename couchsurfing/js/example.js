d0d2a40bae4a3c3e254983e9280c2278b530927f
Robin <robin@reobwuein.nl>
2012-12-02 00:43:46 +0100

    small fixes

--- couchsurfing/js/example.js
+++ couchsurfing/js/example.js
@@ -13,10 +13,14 @@ function init() {
 
     toplist.observe(models.EVENT.CHANGE, function() {
         toplist.results.forEach(function(artist) {
-            console.log(artist.name);
+           // console.log(artist.name);
         });
     });
 
+    var a = models.Artist.fromURI('spotify:artist:5EEw5ABtKN2jJWKvoe7NYU', function(artist) {
+    	  console.log(artist);
+    	});
+    
     toplist.run();
 }
 init();
